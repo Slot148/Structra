1a5293b3d107d16f66d35672a1813aa9574cfde3
Nicolas Anderson <141173875+Slot148@users.noreply.github.com>
2025-03-14 17:42:05 -0300

    Create Component.ts

---
+++ src/Components/Component.ts
@@ -0,0 +1,51 @@
+class Component {
+    element: HTMLElement;
+    children: Component[] = [];
+
+    constructor({
+        tag = 'div',
+        id,
+        classes = [],
+        content,
+        children = [],
+        attributes = {}
+    }: {
+        tag?: string;
+        id?: string;
+        classes?: string[];
+        content?: string;
+        children?: { tag?: string; id?: string; classes?: string[]; content?: string; attributes?: { [key: string]: string }; children?: any[] }[]
+        attributes?: { [key: string]: string };
+    }) {
+
+        this.element = document.createElement(tag);
+        this.element.id = id ||crypto.randomUUID();
+
+        if (classes.length) this.element.classList.add(...classes);
+
+        for (let key in attributes) {
+            this.element.setAttribute(key, attributes[key]);
+        }
+
+        if (content) {
+            const span = document.createElement('span')
+            span.textContent = content
+            this.element.appendChild(span)
+        }
+
+        this.children = children.map((child) => child instanceof Component ? child : new Component(child));
+        this.children.forEach(child => this.element.appendChild(child.element));
+
+        this.render();
+    }  
+
+    render(parent = document.body) {
+        parent.appendChild(this.element);
+        console.log(`Elemento <${this.element.tagName.toLowerCase()}> criado com ID: ${this.element.id}`);
+        return this;
+    }
+
+    destroy() {
+        this.element.remove();
+    }
+}
